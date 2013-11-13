6981bd36a6cc97dcabcbaa988388ed36df4f6955
aspangaro <alexandre.spangaro@gmail.com>
2013-11-13 14:23:54 +0100

    Fix perms

--- admin/importaccountsex.php
+++ admin/importaccountsex.php
@@ -44,11 +44,9 @@ $langs->load("bills");
 $langs->load("main");
 $langs->load("ventilation@ventilation");
 
-if (!$user->rights->ventilation->access) accessforbidden();
-
-// Securite acces client
+// Security check
 if ($user->societe_id > 0) accessforbidden();
-
+if (!$user->rights->accountingex->admin) accessforbidden();
 
 llxHeader('',$langs->trans("ImportAccount"));
 
