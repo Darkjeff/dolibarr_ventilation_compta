994228ea8494741a4c62c69596fec8a9410949ab
Florian Henry <florian.henry@open-concept.pro>
2013-11-20 12:21:08 +0100

    work together

--- admin/account.php
+++ admin/account.php
@@ -20,35 +20,38 @@
  */
 
 /**
- * 		\file       htdocs/accountingex/admin/account.php
- * 		\ingroup    Accounting Expert
- * 		\brief      List accounting account
+ * \file htdocs/accountingex/admin/account.php
+ * \ingroup Accounting Expert
+ * \brief List accounting account
  */
- 
- // Dolibarr environment
- 
-$res=@include("../main.inc.php");
-if (! $res && file_exists("../main.inc.php")) $res=@include("../main.inc.php");
-if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
-if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
-if (! $res) die("Include of main fails");
 
-// Class
+// Dolibarr environment
+$res = @include ("../main.inc.php");
+if (! $res && file_exists ( "../main.inc.php" ))
+	$res = @include ("../main.inc.php");
+if (! $res && file_exists ( "../../main.inc.php" ))
+	$res = @include ("../../main.inc.php");
+if (! $res && file_exists ( "../../../main.inc.php" ))
+	$res = @include ("../../../main.inc.php");
+if (! $res)
+	die ( "Include of main fails" );
+	
+	// Class
 
-dol_include_once ( "/ventilation/class/accountingaccount.class.php");
-dol_include_once ( "/ventilation/class/html.formventilation.class.php");
-
-
+dol_include_once ( "/ventilation/class/accountingaccount.class.php" );
+dol_include_once ( "/ventilation/class/html.formventilation.class.php" );
 
 // langs
-$langs->load("compta");
-$langs->load("ventilation@ventilation");
+$langs->load ( "compta" );
+$langs->load ( "ventilation@ventilation" );
 
 // Security check
-if ($user->societe_id > 0) accessforbidden();
-if (!$user->rights->accountingex->admin) accessforbidden();
-
-//filter
+if ($user->societe_id > 0)
+	accessforbidden ();
+if (! $user->rights->accountingex->admin)
+	accessforbidden ();
+	
+	// filter
 $pcg_version = GETPOST ( "pcg_version", 'alpha' );
 $sortfield = GETPOST ( "sortfield", 'alpha' );
 $sortorder = GETPOST ( "sortorder", 'alpha' );
@@ -58,74 +61,67 @@ if (! $sortfield)
 	$sortfield = "aa.rowid";
 if (! $sortorder)
 	$sortorder = "ASC";
-	
+
 if ($action == 'delete') {
 	$formconfirm = $html->formconfirm ( $_SERVER ["PHP_SELF"] . '?id=' . $id, $langs->trans ( 'DeleteAccount' ), $langs->trans ( 'ConfirmDeleteAccount' ), 'confirm_delete', '', 0, 1 );
 	print $formconfirm;
 }
 
-llxHeader('',$langs->trans("Accounts"));
+llxHeader ( '', $langs->trans ( "Accounts" ) );
 
-print_barre_liste($langs->trans("Accounts"),$page,"account.php","",$sortfield,$sortorder,'',$num);
+print_barre_liste ( $langs->trans ( "Accounts" ), $page, "account.php", "", $sortfield, $sortorder, '', $num );
 
 // $pcgver = $conf->global->ACCOUNTING_PCG_VERSION; pour clause where en fonction du plan comptable
 
 $sql2 = "SELECT aa.rowid, aa.fk_pcg_version, aa.pcg_type, aa.pcg_subtype, aa.account_number, aa.account_parent , aa.label, aa.active ";
 $sql2 .= " FROM " . MAIN_DB_PREFIX . "accountingaccount as aa";
 $sql2 .= " WHERE aa.active = 1";
-if (strlen(trim($_GET["search_account"])))
-{
-	$sql2 .= " AND aa.account_number like '%".$_GET["search_account"]."%'";
+if (strlen ( trim ( $_GET ["search_account"] ) )) {
+	$sql2 .= " AND aa.account_number like '%" . $_GET ["search_account"] . "%'";
 }
-if (strlen(trim($_GET["search_label"])))
-{
-	$sql2 .= " AND aa.label like '%".$_GET["search_label"]."%'";
+if (strlen ( trim ( $_GET ["search_label"] ) )) {
+	$sql2 .= " AND aa.label like '%" . $_GET ["search_label"] . "%'";
 }
-if (strlen(trim($_GET["search_accountparent"])))
-{
-	$sql2 .= " AND aa.account_parent like '%".$_GET["search_accountparent"]."%'";
+if (strlen ( trim ( $_GET ["search_accountparent"] ) )) {
+	$sql2 .= " AND aa.account_parent like '%" . $_GET ["search_accountparent"] . "%'";
 }
-if (strlen(trim($_GET["search_pcgtype"])))
-{
-	$sql2 .= " AND aa.pcg_type like '%".$_GET["search_pcgtype"]."%'";
+if (strlen ( trim ( $_GET ["search_pcgtype"] ) )) {
+	$sql2 .= " AND aa.pcg_type like '%" . $_GET ["search_pcgtype"] . "%'";
 }
-if (strlen(trim($_GET["search_pcgsubtype"])))
-{
-	$sql2 .= " AND aa.pcg_subtype like '%".$_GET["search_pcgsubtype"]."%'";
+if (strlen ( trim ( $_GET ["search_pcgsubtype"] ) )) {
+	$sql2 .= " AND aa.pcg_subtype like '%" . $_GET ["search_pcgsubtype"] . "%'";
 }
 $sql2 .= " ORDER BY $sortfield $sortorder"; // . $db->plimit ( $conf->liste_limit + 1, $offset );
 
-$result = $db->query($sql2);
-if ($result)
-{
-	$num = $db->num_rows($result);
+$result = $db->query ( $sql2 );
+if ($result) {
+	$num = $db->num_rows ( $result );
 	$i = 0;
 	
-		$var = true;
+	$var = true;
 	
 	$param = '';
 	
 	$obj = $db->fetch_object ( $result );
 	
+	print '<form method="GET" action="' . $_SERVER ["PHP_SELF"] . '">';
 	
-  print '<form method="GET" action="' . $_SERVER ["PHP_SELF"] . '">';
-	
-  print '<a class="butAction" href="fiche.php?action=create">'.$langs->trans("Addanaccount").'</a>';
-  print '<a class="butAction" href="importaccounts.php">' . $langs->trans ( "ImportAccount" ) . '</a>';
-  print '<br/><br/>';
+	print '<a class="butAction" href="fiche.php?action=create">' . $langs->trans ( "Addanaccount" ) . '</a>';
+	print '<a class="butAction" href="importaccounts.php">' . $langs->trans ( "ImportAccount" ) . '</a>';
+	print '<br/><br/>';
 	
 	print '<table class="noborder" width="100%">';
 	print '<tr class="liste_titre">';
-	print_liste_field_titre ( $langs->trans ( "AccountNumber" ), "account.php", "aa.account_number", "", $param, "", $sortfield, $sortorder);
-	print_liste_field_titre ( $langs->trans ( "Label" ), "account.php", "aa.label" , "", $param, "", $sortfield, $sortorder );
-	print_liste_field_titre ( $langs->trans ( "Accountparent" ), "account.php", "aa.account_parent" , "", $param, "", $sortfield, $sortorder );
-	print_liste_field_titre ( $langs->trans ( "Pcgtype" ), "account.php", "aa.pcg_type" , "", $param, "", $sortfield, $sortorder );
-	print_liste_field_titre ( $langs->trans ( "Pcgsubtype" ), "account.php", "aa.pcg_subtype", "", $param, "", $sortfield, $sortorder  );
-	print_liste_field_titre ( $langs->trans ( "Active" ), "account.php", "aa.active" , "", $param, "", $sortfield, $sortorder );
+	print_liste_field_titre ( $langs->trans ( "AccountNumber" ), "account.php", "aa.account_number", "", $param, "", $sortfield, $sortorder );
+	print_liste_field_titre ( $langs->trans ( "Label" ), "account.php", "aa.label", "", $param, "", $sortfield, $sortorder );
+	print_liste_field_titre ( $langs->trans ( "Accountparent" ), "account.php", "aa.account_parent", "", $param, "", $sortfield, $sortorder );
+	print_liste_field_titre ( $langs->trans ( "Pcgtype" ), "account.php", "aa.pcg_type", "", $param, "", $sortfield, $sortorder );
+	print_liste_field_titre ( $langs->trans ( "Pcgsubtype" ), "account.php", "aa.pcg_subtype", "", $param, "", $sortfield, $sortorder );
+	print_liste_field_titre ( $langs->trans ( "Active" ), "account.php", "aa.active", "", $param, "", $sortfield, $sortorder );
 	print_liste_field_titre ( "&nbsp;" );
 	print '</tr>';
-
-  print '<tr class="liste_titre">';
+	
+	print '<tr class="liste_titre">';
 	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_account" value="' . GETPOST ( "search_account" ) . '"></td>';
 	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_label" value="' . GETPOST ( "search_label" ) . '"></td>';
 	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_accountparent" value="' . GETPOST ( "search_accountparent" ) . '"></td>';
@@ -136,51 +132,50 @@ if ($result)
 	print '<input type="image" class="liste_titre" src="' . DOL_URL_ROOT . '/theme/' . $conf->theme . '/img/search.png" name="button_search" value="' . dol_escape_htmltag ( $langs->trans ( "Search" ) ) . '" title="' . dol_escape_htmltag ( $langs->trans ( "Search" ) ) . '">';
 	print '</td>';
 	print '</tr>';
-
-
-
-$var = True;
+	
+	$var = True;
+	
+	while ( $i < min ( $num, 1000 ) ) {
+		$obj2 = $db->fetch_object ( $resql2 );
+		$var = ! $var;
 		
-		while ( $i < min ( $num, 1000 ) ) {
-			$obj2 = $db->fetch_object ( $resql2 );
-			$var = ! $var;
-			
-			print "<tr $bc[$var]>";
-
-      print '<td><a href="./fiche.php?id='. $obj2->rowid . '">' . $obj2->account_number . '</td>';
-			print '<td>' . $obj2->label . '</td>';
-			print '<td>' . $obj2->account_parent . '</td>';
-			print '<td>' . $obj2->pcg_type . '</td>';
-			print '<td>' . $obj2->pcg_subtype . '</td>';
-			print '<td>' . $obj2->active . '</td>';
-			
-				print '<td>';
-						if ($user->rights->compta->ventilation->parametrer) {
-							print '<a href="./fiche.php?action=update&id=' . $obj2->rowid  . '">';
-							print img_edit ();
-							print '</a>&nbsp;';
-							print '<a href="./fiche.php?action=delete&id=' . $obj2->rowid  . '">';
-							print img_delete ();
-							print '</a>';
-						}
-						print '</td>' . "\n";
-			
-			
- 
-			print "</tr>";
+		print "<tr $bc[$var]>";
+		
+		print '<td><a href="./fiche.php?id=' . $obj2->rowid . '">' . $obj2->account_number . '</td>';
+		print '<td>' . $obj2->label . '</td>';
+		print '<td>' . $obj2->account_parent . '</td>';
+		print '<td>' . $obj2->pcg_type . '</td>';
+		print '<td>' . $obj2->pcg_subtype . '</td>';
+		print '<td>';
+		if (empty ( $obj2->active )) {
+			print img_picto ( $langs->trans ( "Disabled" ), 'switch_off' );
+		} else {
+			print img_picto ( $langs->trans ( "Activated" ), 'switch_on' );
+		}
+		print '</td>';
+		
+		print '<td>';
+		if ($user->rights->accountingex->admin) {
+			print '<a href="./fiche.php?action=update&id=' . $obj2->rowid . '">';
+			print img_edit ();
+			print '</a>&nbsp;';
+			print '<a href="./fiche.php?action=delete&id=' . $obj2->rowid . '">';
+			print img_delete ();
+			print '</a>';
+		}
+		print '</td>' . "\n";
+		
+		print "</tr>";
 		$i ++;
 	}
-		
+	
 	print "</table>";
-		print '</form>';
-		}
-	else {
+	print '</form>';
+} else {
 	print $db->error ();
 }
 
-	
-	llxFooter ( '' );
-
+llxFooter ( '' );
 
 $db->close ();
 ?>
--- customer/lignes.php
+++ customer/lignes.php
@@ -21,164 +21,145 @@
  */
 
 /**
- * 		\file       htdocs/compta/ventilation/lignes.php
- * 		\ingroup    facture
- * 		\brief      Page de detail des lignes de ventilation d'une facture
+ * \file htdocs/compta/ventilation/lignes.php
+ * \ingroup facture
+ * \brief Page de detail des lignes de ventilation d'une facture
  */
-$res=@include("../main.inc.php");
-if (! $res && file_exists("../main.inc.php")) $res=@include("../main.inc.php");
-if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
-if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
-if (! $res) die("Include of main fails");
+$res = @include ("../main.inc.php");
+if (! $res && file_exists ( "../main.inc.php" ))
+	$res = @include ("../main.inc.php");
+if (! $res && file_exists ( "../../main.inc.php" ))
+	$res = @include ("../../main.inc.php");
+if (! $res && file_exists ( "../../../main.inc.php" ))
+	$res = @include ("../../../main.inc.php");
+if (! $res)
+	die ( "Include of main fails" );
 
-dol_include_once ( "/ventilation/class/html.formventilation.class.php");
-require_once(DOL_DOCUMENT_ROOT."/compta/facture/class/facture.class.php");
-require_once(DOL_DOCUMENT_ROOT."/product/class/product.class.php");
+dol_include_once ( "/ventilation/class/html.formventilation.class.php" );
+require_once (DOL_DOCUMENT_ROOT . "/compta/facture/class/facture.class.php");
+require_once (DOL_DOCUMENT_ROOT . "/product/class/product.class.php");
 
-$langs->load("bills");
-$langs->load("compta");
-$langs->load("main");
-$langs->load("ventilation@ventilation");
+$langs->load ( "bills" );
+$langs->load ( "compta" );
+$langs->load ( "main" );
+$langs->load ( "ventilation@ventilation" );
 
 // Security check
-if ($user->societe_id > 0) accessforbidden();
-if (!$user->rights->accountingex->access) accessforbidden();
+if ($user->societe_id > 0)
+	accessforbidden ();
+if (! $user->rights->accountingex->access)
+	accessforbidden ();
 
-if (empty($_REQUEST['typeid']))
-{
-	$newfiltre=str_replace('filtre=','',$filtre);
-	$filterarray=explode('-',$newfiltre);
-	foreach($filterarray as $val)
-	{
-		$part=explode(':',$val);
-		if ($part[0] == 'c.intitule') $typeid=$part[1];
+if (empty ( $_REQUEST ['typeid'] )) {
+	$newfiltre = str_replace ( 'filtre=', '', $filtre );
+	$filterarray = explode ( '-', $newfiltre );
+	foreach ( $filterarray as $val ) {
+		$part = explode ( ':', $val );
+		if ($part [0] == 'c.intitule')
+			$typeid = $part [1];
 	}
-}
-else
-{
-	$typeid=$_REQUEST['typeid'];
+} else {
+	$typeid = $_REQUEST ['typeid'];
 }
 
 $formventilation = new FormVentilation ( $db );
 
 // change account
 
-if ($action == 'changeaccount') {
+$changeaccount = GETPOST ( 'changeaccount' );
+
+$is_search = GETPOST ( 'button_search_x' );
+
+if (is_array ( $changeaccount ) && count ( $changeaccount ) > 0 && empty ( $is_search )) {
 	$error = 0;
 	
 	$db->begin ();
 	
 	$sql1 = "UPDATE " . MAIN_DB_PREFIX . "facturedet as l";
-	$sql1 .= " SET l.fk_code_ventilation= 1";
-	$sql1 .= " FROM ".MAIN_DB_PREFIX."facture as f";
-  $sql1 .= " , ".MAIN_DB_PREFIX."accountingaccount as aa";
-  $sql1 .= " , ".MAIN_DB_PREFIX."facturedet as l";
-  $sql1 .= " LEFT JOIN ".MAIN_DB_PREFIX."product as p ON p.rowid = l.fk_product";
-  $sql1 .= " WHERE f.rowid = l.fk_facture AND f.fk_statut >= 1 AND l.fk_code_ventilation <> 0 ";
-  $sql1 .= " AND aa.rowid = l.fk_code_ventilation";
-if (strlen(trim($_GET["search_facture"])))
-{
-	$sql1 .= " AND f.facnumber like '%".$_GET["search_facture"]."%'";
-}
-if (strlen(trim($_GET["search_ref"])))
-{
-	$sql1 .= " AND p.ref like '%".$_GET["search_ref"]."%'";
-}
-if (strlen(trim($_GET["search_label"])))
-{
-	$sql1 .= " AND p.label like '%".$_GET["search_label"]."%'";
-}
-if (strlen(trim($_GET["search_account"])))
-{
-	$sql1 .= " AND aa.account_number like '%".$_GET["search_account"]."%'";
-}
-		$resql1 = $db->query ( $sql1 );
-			if (! $resql1) {
-				$error ++;
-				setEventMessage ( $db->lasterror (), 'errors' );
-			}
-						
-				$db->commit ();
-				
-				setEventMessage ( 'compte change', 'mesgs' );
-
+	$sql1 .= " SET l.fk_code_ventilation=" . GETPOST ( 'account_parent' );
+	$sql1 .= ' WHERE l.rowid IN (' . implode ( ',', $changeaccount ) . ')';
+	
+	dol_syslog ( 'htdocs/compta/ventilation/lignes.php::changeaccount sql= ' . $sql1 );
+	$resql1 = $db->query ( $sql1 );
+	if (! $resql1) {
+		$error ++;
+		setEventMessage ( $db->lasterror (), 'errors' );
+	}
+	if (! $error) {
+		$db->commit ();
+		setEventMessage ( $langs->trans ( 'Save' ), 'mesgs' );
 	} else {
-			$db->rollback ();
-			setEventMessage ( $db->lasterror (), 'errors' );
-		}
+		$db->rollback ();
+		setEventMessage ( $db->lasterror (), 'errors' );
+	}
+}
 
-
-
-llxHeader('');
+llxHeader ( '',$langs->trans("CustomersVentilation").' - '.$langs->trans("Dispatched") );
 
 /*
 * Lignes de factures
 *
 */
-$page = $_GET["page"];
-if ($page < 0) $page = 0;
+$page = GETPOST ( "page" );
+if ($page < 0)
+	$page = 0;
 $limit = $conf->liste_limit;
-$offset = $limit * $page ;
+$offset = $limit * $page;
 
 $sql = "SELECT l.rowid , f.facnumber, f.rowid as facid, l.fk_product, l.description, l.total_ht, l.qty, l.tva_tx, l.fk_code_ventilation, aa.label, aa.account_number,";
-$sql.= " p.rowid as product_id, p.ref as product_ref, p.label as product_label, p.fk_product_type as type";
-$sql.= " FROM ".MAIN_DB_PREFIX."facture as f";
-$sql.= " , ".MAIN_DB_PREFIX."accountingaccount as aa";
-$sql.= " , ".MAIN_DB_PREFIX."facturedet as l";
-$sql.= " LEFT JOIN ".MAIN_DB_PREFIX."product as p ON p.rowid = l.fk_product";
-$sql.= " WHERE f.rowid = l.fk_facture AND f.fk_statut >= 1 AND l.fk_code_ventilation <> 0 ";
-$sql.= " AND aa.rowid = l.fk_code_ventilation";
-if (strlen(trim($_GET["search_facture"])))
-{
-	$sql .= " AND f.facnumber like '%".$_GET["search_facture"]."%'";
+$sql .= " p.rowid as product_id, p.ref as product_ref, p.label as product_label, p.fk_product_type as type";
+$sql .= " FROM " . MAIN_DB_PREFIX . "facture as f";
+$sql .= " , " . MAIN_DB_PREFIX . "accountingaccount as aa";
+$sql .= " , " . MAIN_DB_PREFIX . "facturedet as l";
+$sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "product as p ON p.rowid = l.fk_product";
+$sql .= " WHERE f.rowid = l.fk_facture AND f.fk_statut >= 1 AND l.fk_code_ventilation <> 0 ";
+$sql .= " AND aa.rowid = l.fk_code_ventilation";
+if (strlen ( trim ( GETPOST ( "search_facture" ) ) )) {
+	$sql .= " AND f.facnumber like '%" . GETPOST ( "search_facture" ) . "%'";
 }
-if (strlen(trim($_GET["search_ref"])))
-{
-	$sql .= " AND p.ref like '%".$_GET["search_ref"]."%'";
+if (strlen ( trim ( GETPOST ( "search_ref" ) ) )) {
+	$sql .= " AND p.ref like '%" . GETPOST ( "search_ref" ) . "%'";
 }
-if (strlen(trim($_GET["search_label"])))
-{
-	$sql .= " AND p.label like '%".$_GET["search_label"]."%'";
+if (strlen ( trim ( GETPOST ( "search_label" ) ) )) {
+	$sql .= " AND p.label like '%" . GETPOST ( "search_label" ) . "%'";
 }
-if (strlen(trim($_GET["search_account"])))
-{
-	$sql .= " AND aa.account_number like '%".$_GET["search_account"]."%'";
+if (strlen ( trim ( GETPOST ( "search_account" ) ) )) {
+	$sql .= " AND aa.account_number like '%" . GETPOST ( "search_account" ) . "%'";
 }
 if ($typeid) {
-    $sql .= " AND c.intitule=".$typeid;
+	$sql .= " AND c.intitule=" . $typeid;
 }
 
-
 $sql .= " ORDER BY l.rowid DESC";
-$sql .= $db->plimit($limit+1,$offset);
+$sql .= $db->plimit ( $limit + 1, $offset );
 
-$result = $db->query($sql);
+$result = $db->query ( $sql );
 
-if ($result)
-{
-	$num_lignes = $db->num_rows($result);
+if ($result) {
+	$num_lignes = $db->num_rows ( $result );
 	$i = 0;
-
-	print_barre_liste($langs->trans("InvoiceLinesDone"),$page,"lignes.php","",$sortfield,$sortorder,'',$num_lignes);
-
-	print '<form method="GET" action="lignes.php">';
+	
+	print_barre_liste ( $langs->trans ( "InvoiceLinesDone" ), $page, "lignes.php", "", $sortfield, $sortorder, '', $num_lignes );
+	
+	print '<form method="POST" action="lignes.php">';
 	print '<table class="noborder" width="100%">';
 	
-	print '<a class="butAction" href="' . $_SERVER ['PHP_SELF'] . '?action=changeaccount">' . $langs->trans ( "ChangeAccount" ) . '</a>';
-  //print '<div class="inline-block divButAction"><input type="submit" class="butAction" value="' . $langs->trans ( "ChangeAccount" ) . '" /></div>';
-  print '<tr class="liste_titre">';
-	print $formventilation->select_account_parent ( 'account_parent', GETPOST ( 'account_parent' ) );
+	// print '<a class="butAction" href="' . $_SERVER ['PHP_SELF'] . '?action=changeaccount">' . $langs->trans ( "ChangeAccount" ) . '</a>';
+	print '<div class="inline-block divButAction"><input type="submit" class="butAction" value="' . $langs->trans ( "ChangeAccount" ) . '"/></div>';
+	print '<tr class="liste_titre">';
+	print $formventilation->select_account_parent ( GETPOST ( 'account_parent' ), 'account_parent', 1 );
 	
-	print '<tr class="liste_titre"><td>'.$langs->trans("Invoice").'</td>';
-	print '<td>'.$langs->trans("Ref").'</td>';
-	print '<td>'.$langs->trans("Label").'</td>';
-	print '<td>'.$langs->trans("Description").'</td>';
-	print '<td align="left">'.$langs->trans("Amount").'</td>';
-	print '<td colspan="2" align="left">'.$langs->trans("Account").'</td>';
+	print '<tr class="liste_titre"><td>' . $langs->trans ( "Invoice" ) . '</td>';
+	print '<td>' . $langs->trans ( "Ref" ) . '</td>';
+	print '<td>' . $langs->trans ( "Label" ) . '</td>';
+	print '<td>' . $langs->trans ( "Description" ) . '</td>';
+	print '<td align="left">' . $langs->trans ( "Amount" ) . '</td>';
+	print '<td colspan="2" align="left">' . $langs->trans ( "Account" ) . '</td>';
+	print '<td align="center">&nbsp;</td>';
 	print '<td align="center">&nbsp;</td>';
 	print "</tr>\n";
-
-	print '<tr class="liste_titre"><td><input name="search_facture" size="8" value="'.$_GET["search_facture"].'"></td>';
+	
+	print '<tr class="liste_titre"><td><input name="search_facture" size="8" value="' . GETPOST ( "search_facture" ) . '"></td>';
 	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_ref" value="' . GETPOST ( "search_ref" ) . '"></td>';
 	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_label" value="' . GETPOST ( "search_label" ) . '"></td>';
 	print '<td align="right">&nbsp;</td>';
@@ -186,57 +167,59 @@ if ($result)
 	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_account" value="' . GETPOST ( "search_account" ) . '"></td>';
 	print '<td align="center">&nbsp;</td>';
 	print '<td align="right">';
-	print '<input type="image" class="liste_titre" name="button_search" src="'.DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/search.png" alt="'.$langs->trans("Search").'">';
+	print '<input type="image" class="liste_titre" name="button_search" src="' . DOL_URL_ROOT . '/theme/' . $conf->theme . '/img/search.png" alt="' . $langs->trans ( "Search" ) . '">';
 	print '</td>';
+	print '<td align="center">&nbsp;</td>';
 	print "</tr>\n";
-
-	$facture_static=new Facture($db);
-	$product_static=new Product($db);
-
-	$var=True;
-	while ($i < min($num_lignes, $limit))
-	{
-		$objp = $db->fetch_object($result);
-		$var=!$var;
-		$codeCompta = $objp->account_number.' '.$objp->label;
-
+	
+	$facture_static = new Facture ( $db );
+	$product_static = new Product ( $db );
+	
+	$var = True;
+	while ( $i < min ( $num_lignes, $limit ) ) {
+		$objp = $db->fetch_object ( $result );
+		$var = ! $var;
+		$codeCompta = $objp->account_number . ' ' . $objp->label;
+		
 		print "<tr $bc[$var]>";
-
+		
 		// Ref facture
-		$facture_static->ref=$objp->facnumber;
-		$facture_static->id=$objp->facid;
-		print '<td>'.$facture_static->getNomUrl(1).'</td>';
-
+		$facture_static->ref = $objp->facnumber;
+		$facture_static->id = $objp->facid;
+		print '<td>' . $facture_static->getNomUrl ( 1 ) . '</td>';
+		
 		// Ref produit
-		$product_static->ref=$objp->product_ref;
-		$product_static->id=$objp->product_id;
-		$product_static->type=$objp->type;
+		$product_static->ref = $objp->product_ref;
+		$product_static->id = $objp->product_id;
+		$product_static->type = $objp->type;
 		print '<td>';
-		if ($product_static->id) print $product_static->getNomUrl(1);
-		else print '&nbsp;';
+		if ($product_static->id)
+			print $product_static->getNomUrl ( 1 );
+		else
+			print '&nbsp;';
 		print '</td>';
-
-		print '<td>'.dol_trunc($objp->product_label,24).'</td>';
-		print '<td>'.nl2br(dol_trunc($objp->description,32)).'</td>';
-		print '<td align="left">'.price($objp->total_ht).'</td>';
-		print '<td align="left">'.$codeCompta.'</td>';
-		print '<td>'.$objp->rowid.'</td>';
-		print '<td><a href="./fiche.php?id='.$objp->rowid.'">';
-		print img_edit();
+		
+		print '<td>' . dol_trunc ( $objp->product_label, 24 ) . '</td>';
+		print '<td>' . nl2br ( dol_trunc ( $objp->description, 32 ) ) . '</td>';
+		print '<td align="left">' . price ( $objp->total_ht ) . '</td>';
+		print '<td align="left">' . $codeCompta . '</td>';
+		print '<td>' . $objp->rowid . '</td>';
+		print '<td><a href="./fiche.php?id=' . $objp->rowid . '">';
+		print img_edit ();
 		print '</a></td>';
-
+		
+		print '<td align="center"><input type="checkbox" name="changeaccount[]" value="' . $objp->rowid . '"/></td>';
+		
 		print "</tr>";
-		$i++;
+		$i ++;
 	}
-}
-else
-{
-	print $db->error();
+} else {
+	print $db->error ();
 }
 
 print "</table></form>";
 
-$db->close();
+$db->close ();
 
-llxFooter();
+llxFooter ();
 ?>
