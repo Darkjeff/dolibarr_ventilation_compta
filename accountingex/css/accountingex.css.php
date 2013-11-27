0396b81d2a654f727100d4c14f36f2570c76927a
aspangaro <alexandre.spangaro@gmail.com>
2013-11-27 09:02:37 +0100

    Correction et Grand Icone pour les thèmes 2

---
+++ accountingex/css/accountingex.css.php
@@ -0,0 +1,58 @@
+<?php
+/* Copyright (C) 2013      Olivier Geffroy      <jeff@jeffinfo.com>
+ * Copyright (C) 2013      Alexandre Spangaro   <alexandre.spangaro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *		\file       accountingex/css/accountingex.css.php
+ *    \ingroup    Accounting Expert
+ *		\brief      File for CSS style
+ */
+
+session_cache_limiter(FALSE);
+
+ // Dolibarr environment
+$res=@include("../main.inc.php");
+if (! $res && file_exists("../main.inc.php")) $res=@include("../main.inc.php");
+if (! $res && file_exists("../../main.inc.php")) $res=@include("../../main.inc.php");
+if (! $res && file_exists("../../../main.inc.php")) $res=@include("../../../main.inc.php");
+if (! $res) die("Include of main fails");
+
+// Load user to have $user->conf loaded (not done into main because of NOLOGIN constant defined)
+if (empty($user->id) && ! empty($_SESSION['dol_login'])) $user->fetch('',$_SESSION['dol_login']);
+
+// Define css type
+header('Content-type: text/css');
+// Important: Following code is to avoid page request by browser and PHP CPU at
+// each Dolibarr page access.
+if (empty($dolibarr_nocache)) header('Cache-Control: max-age=3600, public, must-revalidate');
+else header('Cache-Control: no-cache');
+
+// On the fly GZIP compression for all pages (if browser support it). Must set the bit 3 of constant to 1.
+if (isset($conf->global->MAIN_OPTIMIZE_SPEED) && ($conf->global->MAIN_OPTIMIZE_SPEED & 0x04)) { ob_start("ob_gzhandler"); }
+
+$path='';    	// This value may be used in future for external module to overwrite theme
+if (! empty($conf->global->MAIN_OVERWRITE_THEME_RES)) { $path='/'.$conf->global->MAIN_OVERWRITE_THEME_RES; }
+
+?>
+
+div.mainmenu.accounting {
+  background-image: url(<?php echo dol_buildpath($path.'/accountingex/img/accountingex.png',1); ?>);
+}
+
+<?php
+if (is_object($db)) $db->close();
+?>
