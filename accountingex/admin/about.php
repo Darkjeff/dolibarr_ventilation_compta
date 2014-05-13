68bb741f15be7de2085119ee8a10fa0b66dcaa28
Darkjeff <jeff@jeffinfo.com>
2014-05-13 08:55:33 +0200

    about update

--- accountingex/admin/about.php
+++ accountingex/admin/about.php
@@ -65,6 +65,18 @@ print '<table class="noborder" width="100%">';
 print '<tr class="liste_titre"><td colspan="2">' . $langs->trans("Authors") . '</td>';
 print '</tr>';
 
+// Olivier Geffroy
+print '<tr><td><img src="../img/jeffinfo.png" width="250"></td>';
+print '<td><b>Olivier Geffroy</b>&nbsp;-&nbsp;Consultant Informatique';
+print '<br>Jeffinfo - 59660 Merville<br>' . $langs->trans("Email") . ' : jeff@jeffinfo.com <br>' . $langs->trans("Phone") . ' : +33 6 08 63 27 40';
+print '<br><a target="_blank" href="http://jeffinfo.com">http://www.jeffinfo.com/</a>';
+print '<br><br><a title="Jeffinfo Facebook" target="_blank" href="https://www.facebook.com/pages/Jeffinfo/165397806821029"><img src="../img/fb.png" width="20"></a>&nbsp;';
+print '<a target="_blank" href="http://twiter.com/zendsi"><img src="../img/tweet.png" width="20"></a>&nbsp;';
+print '<a target="_blank" href="https://plus.google.com/+OlivierGeffroy/posts"><img src="../img/google+.png" width="20"></a>';
+print '</td></tr>';
+
+print '<tr><td>&nbsp;</td></tr>';
+
 // Alexandre Spangaro
 print '<tr><td><img src="../img/fidurex.jpg" width="250"></td>';
 print '<td><b>Alexandre Spangaro</b>&nbsp;-&nbsp;Comptable';
@@ -93,6 +105,22 @@ print '<br>Florian HENRY - Open-Concept.pro  <BR>' . $langs->trans("Email") . ' 
 print '<br><a target="_blank" href="http://www.open-concept.pro">http://www.open-concept.pro</a>';
 print '<br><br><a target="_blank" href="http://twitter.com/_open_concept_"><img src="../img/tweet.png" width="20"></a>';
 print '</td></tr>';
+
+print '<table class="noborder" width="100%">';
+
+print '<tr class="liste_titre"><td colspan="2">' . $langs->trans("Investor") . '</td>';
+print '</tr>';
+
+print '<tr><td>Jeffinfo</td>';
+print '<td>2000 €</td></tr>';
+print '<tr><td>Nord Anim</td>';
+print '<td>120 €</td></tr>';
+print '<tr><td>Hydroflex</td>';
+print '<td>120 €</td></tr>';
+print '<tr><td>Asysteo</td>';
+print '<td>120 €</td></tr>';
+print '<tr><td>Fournisseur Medical</td>';
+print '<td>120 €</td></tr>';
 
 dol_htmloutput_mesg($mesg);
 
