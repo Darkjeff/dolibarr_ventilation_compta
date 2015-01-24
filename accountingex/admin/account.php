8a9dcb0b310e59ab9e5e49c8290c429141cecd6b
aspangaro <alexandre.spangaro@gmail.com>
2015-01-24 19:58:14 +0100

    Add function to purge search criteria in list of accounts

--- accountingex/admin/account.php
+++ accountingex/admin/account.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2013-2014 Olivier Geffroy      <jeff@jeffinfo.com>
- * Copyright (C) 2013-2014 Alexandre Spangaro   <alexandre.spangaro@gmail.com>
+ * Copyright (C) 2013-2015 Alexandre Spangaro   <alexandre.spangaro@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@ if (! $res && file_exists("../../../main.inc.php"))
 if (! $res)
 	die("Include of main fails");
 	
-	// Class
+// Class
 dol_include_once("/accountingex/core/lib/account.lib.php");
 dol_include_once("/accountingex/class/accountingaccount.class.php");
 dol_include_once("/accountingex/class/html.formventilation.class.php");
@@ -95,6 +95,22 @@ if ($action == 'disable') {
 	}
 }
 
+$search_account=GETPOST("search_account");
+$search_label=GETPOST("search_label");
+$search_accountparent=GETPOST("search_accountparent");
+$search_pcgtype=GETPOST("search_pcgtype");
+$search_pcgsubtype=GETPOST("search_pcgsubtype");
+
+// Purge criteria
+if (GETPOST("button_removefilter_x") || GETPOST("button_removefilter")) // Both test are required to be compatible with all browsers
+{
+	$search_account="";
+	$search_label="";
+	$search_accountparent="";
+    $search_pcgtype="";
+    $search_pcgsubtype="";
+}
+
 /*
  * View
  *
@@ -108,20 +124,20 @@ $sql .= " FROM " . MAIN_DB_PREFIX . "accountingaccount as aa, " . MAIN_DB_PREFIX
 $sql .= " WHERE aa.fk_pcg_version = asy.pcg_version";
 $sql .= " AND asy.rowid = " . $pcgver;
 
-if (strlen(trim($_GET["search_account"]))) {
-	$sql .= " AND aa.account_number like '%" . $_GET["search_account"] . "%'";
+if (strlen(trim($search_account))) {
+	$sql .= " AND aa.account_number like '%" . $search_account . "%'";
 }
-if (strlen(trim($_GET["search_label"]))) {
-	$sql .= " AND aa.label like '%" . $_GET["search_label"] . "%'";
+if (strlen(trim($search_label))) {
+	$sql .= " AND aa.label like '%" . $search_label . "%'";
 }
-if (strlen(trim($_GET["search_accountparent"]))) {
-	$sql .= " AND aa.account_parent like '%" . $_GET["search_accountparent"] . "%'";
+if (strlen(trim($search_accountparent))) {
+	$sql .= " AND aa.account_parent like '%" . $search_accountparent . "%'";
 }
-if (strlen(trim($_GET["search_pcgtype"]))) {
-	$sql .= " AND aa.pcg_type like '%" . $_GET["search_pcgtype"] . "%'";
+if (strlen(trim($search_pcgtype))) {
+	$sql .= " AND aa.pcg_type like '%" . $search_pcgtype . "%'";
 }
-if (strlen(trim($_GET["search_pcgsubtype"]))) {
-	$sql .= " AND aa.pcg_subtype like '%" . $_GET["search_pcgsubtype"] . "%'";
+if (strlen(trim($search_pcgsubtype))) {
+	$sql .= " AND aa.pcg_subtype like '%" . $search_pcgsubtype . "%'";
 }
 
 $sql .= $db->order($sortfield, $sortorder);
@@ -147,25 +163,26 @@ if ($result) {
 	
 	print '<table class="noborder" width="100%">';
 	print '<tr class="liste_titre">';
-	print_liste_field_titre($langs->trans("AccountNumber"), "account.php", "aa.account_number", "", $param, "", $sortfield, $sortorder);
-	print_liste_field_titre($langs->trans("Label"), "account.php", "aa.label", "", $param, "", $sortfield, $sortorder);
-	print_liste_field_titre($langs->trans("Accountparent"), "account.php", "aa.account_parent", "", $param, "", $sortfield, $sortorder);
-	print_liste_field_titre($langs->trans("Pcgtype"), "account.php", "aa.pcg_type", "", $param, "", $sortfield, $sortorder);
-	print_liste_field_titre($langs->trans("Pcgsubtype"), "account.php", "aa.pcg_subtype", "", $param, "", $sortfield, $sortorder);
-	print_liste_field_titre($langs->trans("Active"), "account.php", "aa.active", "", $param, "", $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans("AccountNumber"), $_SERVER["PHP_SELF"], "aa.account_number", "", $param, "", $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans("Label"), $_SERVER["PHP_SELF"], "aa.label", "", $param, "", $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans("Accountparent"), $_SERVER["PHP_SELF"], "aa.account_parent", "", $param, "", $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans("Pcgtype"), $_SERVER["PHP_SELF"], "aa.pcg_type", "", $param, "", $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans("Pcgsubtype"), $_SERVER["PHP_SELF"], "aa.pcg_subtype", "", $param, "", $sortfield, $sortorder);
+	print_liste_field_titre($langs->trans("Active"), $_SERVER["PHP_SELF"], "aa.active", "", $param, "", $sortfield, $sortorder);
 	print_liste_field_titre("&nbsp;");
 	print '</tr>';
 	
 	print '<tr class="liste_titre">';
-	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_account" value="' . GETPOST("search_account") . '"></td>';
-	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_label" value="' . GETPOST("search_label") . '"></td>';
-	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_accountparent" value="' . GETPOST("search_accountparent") . '"></td>';
-	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_pcgtype" value="' . GETPOST("search_pcgtype") . '"></td>';
-	print '<td class="liste_titre"><input type="text" class="flat" size="15" name="search_pcgsubtype" value="' . GETPOST("search_pcgsubtype") . '"></td>';
+	print '<td class="liste_titre"><input type="text" class="flat" size="10" name="search_account" value="' . $search_account . '"></td>';
+	print '<td class="liste_titre"><input type="text" class="flat" size="20" name="search_label" value="' . $search_label . '"></td>';
+	print '<td class="liste_titre"><input type="text" class="flat" size="10" name="search_accountparent" value="' . $search_accountparent . '"></td>';
+	print '<td class="liste_titre"><input type="text" class="flat" size="10" name="search_pcgtype" value="' . $search_pcgtype . '"></td>';
+	print '<td class="liste_titre"><input type="text" class="flat" size="10" name="search_pcgsubtype" value="' . $search_pcgsubtype . '"></td>';
 	print '<td class="liste_titre">&nbsp;</td>';
-	print '<td class="liste_titre" align="right">';
-	print '<input type="image" class="liste_titre" src="' . DOL_URL_ROOT . '/theme/' . $conf->theme . '/img/search.png" name="button_search" value="' . dol_escape_htmltag($langs->trans("Search")) . '" title="' . dol_escape_htmltag($langs->trans("Search")) . '">';
-	print '</td>';
+	
+    print '<td class="liste_titre" align="right"><input class="liste_titre" type="image" src="'.img_picto($langs->trans("Search"),'search.png','','',1).'" value="'.dol_escape_htmltag($langs->trans("Search")).'" title="'.dol_escape_htmltag($langs->trans("Search")).'">';
+    print '<input type="image" class="liste_titre" name="button_removefilter" src="'.img_picto($langs->trans("Search"),'searchclear.png','','',1).'" value="'.dol_escape_htmltag($langs->trans("RemoveFilter")).'" title="'.dol_escape_htmltag($langs->trans("RemoveFilter")).'">';
+    print '</td>';
 	print '</tr>';
 	
 	$var = True;
@@ -214,5 +231,4 @@ if ($result) {
 }
 
 llxFooter();
-
 $db->close();
