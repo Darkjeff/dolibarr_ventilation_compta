0225396d6ae5297380891d7abf2414f98e0cefe3
Alexandre SPANGARO <alexandre.spangaro@gmail.com>
2017-03-21 06:50:59 +0100

    Begin to integrate Various_payment

--- htdocs/accountancy/sql/update.sql
+++ htdocs/accountancy/sql/update.sql
@@ -3,3 +3,23 @@ ALTER TABLE llx_accounting_bookkeeping ADD COLUMN fk_user_modif     integer;
 ALTER TABLE llx_accounting_bookkeeping ADD COLUMN date_creation		datetime;
 ALTER TABLE llx_accounting_bookkeeping ADD COLUMN tms               timestamp  NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 ALTER TABLE llx_expensereport_det ADD COLUMN fk_code_ventilation integer DEFAULT 0;
+
+create table llx_payment_various
+(
+  rowid                 integer AUTO_INCREMENT PRIMARY KEY,
+  tms                   timestamp,
+  datec                 datetime,
+  datep                 date,
+  datev                 date,
+  sens                  smallint DEFAULT 0 NOT NULL,
+  amount                double(24,8) DEFAULT 0 NOT NULL,
+  fk_typepayment        integer NOT NULL,
+  num_payment           varchar(50),
+  label                 varchar(255),
+  accountancy_code		varchar(32),
+  entity                integer DEFAULT 1 NOT NULL,
+  note                  text,
+  fk_bank               integer,
+  fk_user_author        integer,
+  fk_user_modif         integer
+)ENGINE=innodb;
